3cf10106cdb6f176a5cea028249cf339ddbd36c0
David MacMahon <davidm@astro.berkeley.edu>
2011-12-01 16:13:53 -0800

    Add xgpuinfo utility to show library build options
    Here is some sample output:
    
    $ xgpuinfo
    Number of polarizations: 2
    Number of stations: 256
    Number of baselines: 32896
    Number of frequencies: 10
    Number of time samples per GPU integration: 1000
    Number of time samples per transfer to GPU: 100
    Type of ComplexInput components: 8 bit integers
    Number of ComplexInput elements in input vector: 5120000
    Number of ComplexInput elements per transfer to GPU: 512000
    Number of Complex elements in output vector: 1320960
    Output matrix order: register tile triangular

---
+++ src/xgpuinfo.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+#include "xgpu.h"
+
+int main(int argc, char** argv)
+{
+
+  XGPUInfo xgpu_info;
+
+  xgpuInfo(&xgpu_info);
+
+  printf("Number of polarizations: %u\n", xgpu_info.npol);
+  printf("Number of stations: %u\n", xgpu_info.nstation);
+  printf("Number of baselines: %u\n", xgpu_info.nbaseline);
+  printf("Number of frequencies: %u\n", xgpu_info.nfrequency);
+
+  printf("Number of time samples per GPU integration: %u\n",
+      xgpu_info.ntime);
+
+  printf("Number of time samples per transfer to GPU: %u\n",
+      xgpu_info.ntimepipe);
+
+  printf("Type of ComplexInput components: ");
+  switch(xgpu_info.input_type) {
+    case XGPU_INT8:    printf("8 bit integers\n"); break;
+    case XGPU_FLOAT32: printf("32 bit floats\n"); break;
+    case XGPU_INT32:   printf("32 bit integers\n"); break;
+    default: printf("<unknown type code: %d>\n", xgpu_info.input_type);
+  }
+
+  printf("Number of ComplexInput elements in input vector: %llu\n",
+      xgpu_info.vecLength);
+
+  printf("Number of ComplexInput elements per transfer to GPU: %llu\n",
+      xgpu_info.vecLengthPipe);
+
+  printf("Number of Complex elements in output vector: %llu\n",
+      xgpu_info.matLength);
+
+  printf("Output matrix order: ");
+  switch(xgpu_info.matrix_order) {
+    case TRIANGULAR_ORDER:               printf("triangular\n");
+                                         break;
+    case REAL_IMAG_TRIANGULAR_ORDER:     printf("real imaginary triangular\n");
+                                         break;
+    case REGISTER_TILE_TRIANGULAR_ORDER: printf("register tile triangular\n");
+                                         break;
+    default: printf("<unknown order code: %d>\n", xgpu_info.matrix_order);
+  }
+
+  return 0;
+}
