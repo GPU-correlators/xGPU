c3749af5ffb69df6cd6b9bfce9e4a2407ec17eb0
David MacMahon <davidm@astro.berkeley.edu>
2012-04-30 10:42:34 -0700

    Support complex block size of 8 in CPU correlator

--- src/omp_xengine.c
+++ src/omp_xengine.c
@@ -24,6 +24,12 @@ do {                                                                            
   acc.imag += (float)z0.imag * (float)z1.real - (float)z0.real * (float)z1.imag; \
 } while (0)
 
+#ifndef COMPLEX_BLOCK_SIZE
+#define COMPLEX_BLOCK_SIZE 1
+#elif COMPLEX_BLOCK_SIZE != 1 && COMPLEX_BLOCK_SIZE != 8
+#error COMPLEX_BLOCK_SIZE must be 1 or 8
+#endif
+
 void xgpuOmpXengine(Complex *matrix_h, ComplexInput *array_h) {
   int num_procs = omp_get_num_procs();
   #pragma omp parallel num_threads(num_procs)
@@ -41,10 +47,30 @@ void xgpuOmpXengine(Complex *matrix_h, ComplexInput *array_h) {
       Complex sumYY; sumYY.real = 0.0; sumYY.imag = 0.0;
       ComplexInput inputRowX, inputRowY, inputColX, inputColY;
       for(t=0; t<NTIME; t++){
+#if COMPLEX_BLOCK_SIZE == 1
 	inputRowX = array_h[((t*NFREQUENCY + f)*NSTATION + station1)*NPOL];
 	inputRowY = array_h[((t*NFREQUENCY + f)*NSTATION + station1)*NPOL + 1];
 	inputColX = array_h[((t*NFREQUENCY + f)*NSTATION + station2)*NPOL];
 	inputColY = array_h[((t*NFREQUENCY + f)*NSTATION + station2)*NPOL + 1];
+#else
+	// Probably not the cleanest way to do this...
+	int i1 = ((t*NFREQUENCY + f)*NSTATION + station1)*NPOL;
+	int i2 = ((t*NFREQUENCY + f)*NSTATION + station2)*NPOL;
+	i1 = 8*(i1/8) + ((i1/2)%4);
+	i2 = 8*(i2/8) + ((i2/2)%4);
+	ComplexInput rowXYreal = array_h[i1];
+	ComplexInput rowXYimag = array_h[i1+4];
+	ComplexInput colXYreal = array_h[i2];
+	ComplexInput colXYimag = array_h[i2+4];
+	inputRowX.real = rowXYreal.real;
+	inputRowX.imag = rowXYimag.real;
+	inputRowY.real = rowXYreal.imag;
+	inputRowY.imag = rowXYimag.imag;
+	inputColX.real = colXYreal.real;
+	inputColX.imag = colXYimag.real;
+	inputColY.real = colXYreal.imag;
+	inputColY.imag = colXYimag.imag;
+#endif
 	cxmac(sumXX, inputRowX, inputColX);
 	cxmac(sumXY, inputRowX, inputColY);
 	cxmac(sumYX, inputRowY, inputColX);
